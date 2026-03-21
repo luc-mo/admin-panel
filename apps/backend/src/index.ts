d4ba392e53e14ae748abad29e5c87dece70091c3
SnowDrive <luc.moralesr@gmail.com>
2026-03-21 01:03:31 -0300

    feat: init backend app

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,1 @@
+console.log('Hello, Princesitas Backend!')
---
+++ apps/backend/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+	entry: ['src/index.ts'],
+	format: ['esm'],
+	outExtension: () => ({ js: '.js' }),
+	tsconfig: './tsconfig.json',
+	minify: false,
+	clean: true,
+	treeshake: true,
+})
