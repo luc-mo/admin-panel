e657d64bb3cf3e3c5654af7950758b59039a3caa
SnowDrive <luc.moralesr@gmail.com>
2026-03-24 14:43:09 -0300

    feat: add multiple methods to permission repository

--- apps/backend/src/infrastructure/persistence/permission/repository.ts
+++ apps/backend/src/infrastructure/persistence/permission/repository.ts
@@ -1,3 +1,4 @@
+import { Filter } from 'firebase-admin/firestore'
 import { Logger } from '@snowdrive/logger'
 import { InjectableDependency } from '@/shared/injectable-dependency'
 import type { Permission } from '@princesitas/core'
@@ -8,16 +9,44 @@ export class PermissionRepository extends InjectableDependency(
 	'dbHandler',
 	'permissionDocumentParser'
 ) {
+	public async count() {
+		const collection = this._getCollection()
+		const snapshot = await collection.count().get()
+		return snapshot.data().count
+	}
+
+	public async find(limit: number, offset: number) {
+		const collection = this._getCollection()
+		const documents = await collection.limit(limit).offset(offset).get()
+		return documents.docs.map((document) =>
+			this._permissionDocumentParser.toDomain(document.data())
+		)
+	}
+
 	public async findById(id: string) {
 		const collection = this._getCollection()
 		const document = await collection.doc(id).get()
 		return document.exists ? this._permissionDocumentParser.toDomain(document.data()!) : null
 	}
 
+	public async findByIdOrKey(id: string, key: string) {
+		const collection = this._getCollection()
+		const query = Filter.or(Filter.where('id', '==', id), Filter.where('key', '==', key))
+		const document = await collection.where(query).get()
+		return document.docs[0]
+			? this._permissionDocumentParser.toDomain(document.docs[0].data())
+			: null
+	}
+
 	public async save(permission: Permission) {
 		const collection = this._getCollection()
 		const document = this._permissionDocumentParser.toDocument(permission)
 		await collection.doc(permission.id).set(document)
+	}
+
+	public async remove(id: string) {
+		const collection = this._getCollection()
+		await collection.doc(id).delete()
 	}
 
 	private _getCollection() {
