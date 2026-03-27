2eb30f56a275df0e4ee9ba526e2155419ca66b04
SnowDrive <luc.moralesr@gmail.com>
2026-03-27 08:15:45 -0300

    feat: add frontend config file

---
+++ apps/frontend/src/shared/config/index.ts
@@ -0,0 +1,7 @@
+export const config = {
+	firebase: {
+		projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+		appId: import.meta.env.VITE_FIREBASE_APP_ID,
+		apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	},
+}
