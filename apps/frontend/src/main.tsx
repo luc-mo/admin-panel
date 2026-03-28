e0dfc6552e3accac63fd1e47ecc895d24a6c43ab
SnowDrive <luc.moralesr@gmail.com>
2026-03-28 20:57:00 -0300

    feat: add and design icons and css in js, add global styles and antd layers

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,6 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { StyleProvider } from '@ant-design/cssinjs'
+import { ConfigProvider } from 'antd'
 import { router } from './router.tsx'
 
 const root = document.getElementById('root')!
-createRoot(root).render(<RouterProvider router={router} />)
+createRoot(root).render(
+	<StyleProvider layer>
+		<ConfigProvider>
+			<RouterProvider router={router} />
+		</ConfigProvider>
+	</StyleProvider>
+)
