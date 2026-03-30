2a1030300cf50160333899de7b062b99421fd7c9
SnowDrive <luc.moralesr@gmail.com>
2026-03-29 23:07:49 -0300

    feat: add api base url to config file

--- apps/frontend/src/config/index.ts
+++ apps/frontend/src/config/index.ts
@@ -1,4 +1,7 @@
 export const config = {
+	app: {
+		apiBaseUrl: import.meta.env.VITE_API_BASE_URL,
+	},
 	firebase: {
 		projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
 		appId: import.meta.env.VITE_FIREBASE_APP_ID,
