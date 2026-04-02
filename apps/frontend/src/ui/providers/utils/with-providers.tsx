b0c4872734e5cd3f982662b35891c3dd66abca68
SnowDrive <luc.moralesr@gmail.com>
2026-04-02 00:45:06 -0300

    feat: simplify with providers hook to avoid unnecesary component tree recreation

--- apps/frontend/src/ui/providers/utils/with-providers.tsx
+++ apps/frontend/src/ui/providers/utils/with-providers.tsx
@@ -1,32 +1,13 @@
-import type { IProvider, IProvidersProps } from './types'
+import type { IProvider } from './types'
 
 export function withProviders<
 	Ps extends readonly IProvider<unknown>[],
 	ComponentProps extends object = object,
->(providers: Ps, Component: React.FC<ComponentProps & IProvidersProps<Ps>>) {
-	const buildInjector = (
-		props: ComponentProps,
-		index = 0,
-		accumulator: Record<string, any> = {}
-	) => {
-		if (index === providers.length) {
-			return <Component {...props} {...(accumulator as IProvidersProps<Ps>)} />
-		}
-
-		const CurrentProvider = providers[index]
-		const Injector: React.FC = () => {
-			const value = CurrentProvider.use()
-			const newIndex = index + 1
-			const newAccumulator = { ...accumulator, [CurrentProvider.contextName]: value }
-			return buildInjector(props, newIndex, newAccumulator)
-		}
-		return <Injector />
-	}
-
+>(providers: Ps, Component: React.FC<ComponentProps>) {
 	return (props: ComponentProps) => {
 		return providers.reduceRight(
 			(children, { providerName, Provider }) => <Provider key={providerName}>{children}</Provider>,
-			buildInjector(props)
+			<Component {...props} />
 		)
 	}
 }
