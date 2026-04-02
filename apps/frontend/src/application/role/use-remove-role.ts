2174e72056f786c83b269b83d2b8607c1fb6b9c5
SnowDrive <luc.moralesr@gmail.com>
2026-04-02 03:24:54 -0300

    feat: add use find roles and use remove role use cases

---
+++ apps/frontend/src/application/role/use-remove-role.ts
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import type { ICoreServicesContext } from '@/ui/providers/core-services-provider'
+import type { IToastContext } from '@/ui/providers/toast-provider'
+
+interface IUseRemoveRoleProps {
+	coreServices: ICoreServicesContext
+	toast: IToastContext
+}
+
+export const useRemoveRole = ({ coreServices, toast }: IUseRemoveRoleProps) => {
+	const [loading, setLoading] = useState(false)
+
+	const execute = async (id: string) => {
+		try {
+			setLoading(true)
+			await coreServices.roleService.removeRole({ id })
+			toast.show('success', 'Rol eliminado correctamente')
+		} catch {
+			toast.show('error', 'Ocurrió un error al eliminar el rol')
+		}
+		setLoading(false)
+	}
+
+	return { loading, execute }
+}
