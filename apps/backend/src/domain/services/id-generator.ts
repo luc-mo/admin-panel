f9540b9f706fd13f7ee845f32355892bce9bb6d7
SnowDrive <luc.moralesr@gmail.com>
2026-04-19 18:22:16 -0400

    feat: add md5 method to id generator service

--- apps/backend/src/domain/services/id-generator.ts
+++ apps/backend/src/domain/services/id-generator.ts
@@ -4,4 +4,8 @@ export class IdGenerator {
 	public uuid(): string {
 		return crypto.randomUUID()
 	}
+
+	public md5(input: string): string {
+		return crypto.createHash('md5').update(input).digest('hex')
+	}
 }
